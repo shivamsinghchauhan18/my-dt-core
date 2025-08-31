5d057f091090b0bdc45c44e0da600f90e5755094
shivamsinghchauhan18 <shivam.chauhan@tum.de>
2025-08-31 02:26:55 +0200

    OpenCV fixed

--- scripts/10_camera_decode.sh
+++ scripts/10_camera_decode.sh
@@ -11,7 +11,8 @@ source_ws
 
 extra_args=()
 if [ "${FORCE_OPENCV:-}" = "1" ] || [ "${FORCE_OPENCV:-}" = "true" ]; then
-	extra_args+=( decoder_node/force_opencv:=true )
+	# Pass fully-qualified private param so it applies under the robot namespace
+	extra_args+=( "/${VEHICLE_NAME}/decoder_node/force_opencv:=true" )
 fi
 
 roslaunch_echo duckietown_demos image_decoding.launch veh:="${VEHICLE_NAME}" "${extra_args[@]}"
